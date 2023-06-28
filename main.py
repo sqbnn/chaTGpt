fd37bc5400a3d3eba1ae078b7f19264663177cc8
Ep1k3 <erevantsyan2019@mail.ru>
2023-06-28 14:50:17 +0300

    new code

--- main.py
+++ main.py
@@ -9,34 +9,61 @@ from dotenv import load_dotenv
 import os
 import openai
 
-openai.api_key = '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'
-openai.Model.list()
-
 main = ReplyKeyboardMarkup(resize_keyboard=True)
-main.add('Что стало с первым ботом?').add('Че там по It, когда 300к в секунду?')
+main.add('Точный').add('Сбалансированный').add('Творческий')
 
 
 load_dotenv()
+
+openai.api_key = 'API_TOKEN'
+openai.Model.list()
+
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer(f'{message.from_user.id}')
-    await message.answer(f'{message.from_user.first_name}, ',
-                         reply_markup=main)
+    await message.answer(f'Привет, ', reply_markup=main)
 
-@dp.message_handler()
+@dp.message_handler(text='Точный')
 async def answer(message: types.Message):
     response = openai.Completion.create(
         model='text-davinci-003',
         prompt=message.text,
-        temperature=1,
+        temperature=0.2,
         max_tokens=2048,
         top_p=0.7,
         frequency_penalty=0
     )
     await message.reply(response['choices'][0]['text'])
+
+@dp.message_handler(text='Сбалансированный')
+async def answer(message: types.Message):
+    response = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=message.text,
+        temperature=0.5,
+        max_tokens=2048,
+        top_p=0.7,
+        frequency_penalty=0
+    )
+    await message.reply(response['choices'][0]['text'])
+
+@dp.message_handler(text='Творческий')
+async def answer(message: types.Message):
+    response = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=message.text,
+        temperature=0.9,
+        max_tokens=2048,
+        top_p=0.7,
+        frequency_penalty=0
+    )
+    await message.reply(response['choices'][0]['text'])
+
+@dp.message_handler()
+async def cmd_start(message: types.Message):
+    await message.reply('Я тебя не понимаю')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
